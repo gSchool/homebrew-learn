fb667160e116677e20d8b95987ee51bb12c01e55
goreleaserbot <goreleaser@carlosbecker.com>
2020-01-23 10:06:30 -0700

    Brew formula update for glearn-cli version v0.6.9

--- Formula/learn.rb
+++ Formula/learn.rb
@@ -2,16 +2,16 @@
 class Learn < Formula
   desc "Learn curriculum publishing tool © Galvanize, Inc."
   homepage ""
-  version "0.6.8"
+  version "0.6.9"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gSchool/glearn-cli/releases/download/v0.6.8/glearn-cli_0.6.8_Darwin_x86_64.tar.gz"
-    sha256 "d720fc68b11eeabac17af25cbd24529f9283754ce85c6b416dd780de555f66e8"
+    url "https://github.com/gSchool/glearn-cli/releases/download/v0.6.9/glearn-cli_0.6.9_Darwin_x86_64.tar.gz"
+    sha256 "6dc2d71de7c58221e6646fb5849ae0f1b9e4251f70c6cd781947ea7cd4616976"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gSchool/glearn-cli/releases/download/v0.6.8/glearn-cli_0.6.8_Linux_x86_64.tar.gz"
-      sha256 "806a6eaff318b9585bb8eb3f84dab1d5e431ed261d56dbf143b97eb009b8d2d5"
+      url "https://github.com/gSchool/glearn-cli/releases/download/v0.6.9/glearn-cli_0.6.9_Linux_x86_64.tar.gz"
+      sha256 "2b457596afa7e139fc1ba1cbdb9aadccaadbf435c7c5e57204519f8470bf9d62"
     end
   end
   
