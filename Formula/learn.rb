1261d2a6d6fa1b2b8e3ed6b5635c5058473cfea3
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-16 16:31:03 -0600

    Brew formula update for glearn-cli version v0.9.0

--- Formula/learn.rb
+++ Formula/learn.rb
@@ -10,23 +10,23 @@ class Learn < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/pabranch/glearn-cli/releases/download/v0.9.0/glearn-cli_0.9.0_Darwin_x86_64.tar.gz"
-      sha256 "bba7fac6a65b492fdf5e9ddd621edc4367cdf7e646a0d267d7508cb85ae54f10"
+      url "https://github.com/gSchool/glearn-cli/releases/download/v0.9.0/glearn-cli_0.9.0_Darwin_x86_64.tar.gz"
+      sha256 "faed792236877af8908c9178061f7caaada8a63fa5ba2d5c3bdf813a7e6ecc47"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/pabranch/glearn-cli/releases/download/v0.9.0/glearn-cli_0.9.0_Darwin_arm64.tar.gz"
-      sha256 "7e94990b4943b0d196fa29cf004d08003013141b2655fcef13c565222b6c3a81"
+      url "https://github.com/gSchool/glearn-cli/releases/download/v0.9.0/glearn-cli_0.9.0_Darwin_arm64.tar.gz"
+      sha256 "24577deb58bf9f92bc196e5b928b3456641da8b80e11cb370ec887349004367c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/pabranch/glearn-cli/releases/download/v0.9.0/glearn-cli_0.9.0_Linux_x86_64.tar.gz"
-      sha256 "c3bb7d5dd57e1298f6c1103684aff4f3b9c57fc6d875be418140eb538cd16d7c"
+      url "https://github.com/gSchool/glearn-cli/releases/download/v0.9.0/glearn-cli_0.9.0_Linux_x86_64.tar.gz"
+      sha256 "6247f1d5d72054d02bc86c4c9db5d60e0bb8220387cd6c79844113c94f761c2e"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pabranch/glearn-cli/releases/download/v0.9.0/glearn-cli_0.9.0_Linux_arm64.tar.gz"
-      sha256 "b094317769266258a046bb1e256c872e64be6dafe822e072cbef27f79d03796c"
+      url "https://github.com/gSchool/glearn-cli/releases/download/v0.9.0/glearn-cli_0.9.0_Linux_arm64.tar.gz"
+      sha256 "e121b35410690c2d2d2c2327f0c56c2293e0d7e97ccbe1341811ea9ebaf71159"
     end
   end
 
