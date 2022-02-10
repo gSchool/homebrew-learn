d6ce203fa4f555d2315786ca5a0946b2660cdffb
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-10 15:10:14 -0700

    Brew formula update for glearn-cli version v0.10.0

--- Formula/learn.rb
+++ Formula/learn.rb
@@ -5,20 +5,20 @@
 class Learn < Formula
   desc "Learn curriculum publishing tool © Galvanize, Inc."
   homepage ""
-  version "0.9.4"
+  version "0.10.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gSchool/glearn-cli/releases/download/v0.9.4/glearn-cli_0.9.4_Darwin_arm64.tar.gz"
-      sha256 "502c06bd91b0de9fcfeb9c8dd9231c60ab929b0c01eabb4a8810003fccc4b06d"
+      url "https://github.com/gSchool/glearn-cli/releases/download/v0.10.0/glearn-cli_0.10.0_Darwin_arm64.tar.gz"
+      sha256 "c4049677e65f8725554986879c5dc2eed64e70af30f278a8de9f4818ff2e7392"
 
       def install
         bin.install "learn"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/gSchool/glearn-cli/releases/download/v0.9.4/glearn-cli_0.9.4_Darwin_x86_64.tar.gz"
-      sha256 "bfe29d8a6ed5452d60227d2ef431f744c92edf40c6402fc696137bf08d177c8a"
+      url "https://github.com/gSchool/glearn-cli/releases/download/v0.10.0/glearn-cli_0.10.0_Darwin_x86_64.tar.gz"
+      sha256 "d5295e4167e49c2948a8333949f3cc61a6ae8061091224c248dc9bab8841435b"
 
       def install
         bin.install "learn"
@@ -28,16 +28,16 @@ class Learn < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/gSchool/glearn-cli/releases/download/v0.9.4/glearn-cli_0.9.4_Linux_arm64.tar.gz"
-      sha256 "38882f23d8915e4b6924fddc105b2f7f9a6fff7e3695d9edb1db3afbe13948ea"
+      url "https://github.com/gSchool/glearn-cli/releases/download/v0.10.0/glearn-cli_0.10.0_Linux_arm64.tar.gz"
+      sha256 "ff34bb2214ce25de7ec0715af2a5dd5938924b40d85e196f66192994771875bb"
 
       def install
         bin.install "learn"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/gSchool/glearn-cli/releases/download/v0.9.4/glearn-cli_0.9.4_Linux_x86_64.tar.gz"
-      sha256 "9eb9f483960c2becc7f61f5105a2241c940f15d8c34a6baa5c7a4e727a0b686a"
+      url "https://github.com/gSchool/glearn-cli/releases/download/v0.10.0/glearn-cli_0.10.0_Linux_x86_64.tar.gz"
+      sha256 "5e1f8177230a7f0312753652d80135437d2ea25788efb38c04c366143ac36d9c"
 
       def install
         bin.install "learn"
